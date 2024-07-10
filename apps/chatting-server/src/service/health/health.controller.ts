1bab9a0ad926fdd140207be38f4043337c14827a
seungminkim <ksm8797@naver.com>
2024-07-10 23:11:35 +0900

    feature : add health service and  @GET 'health' API

--- apps/chatting-server/src/service/health/health.controller.ts
+++ apps/chatting-server/src/service/health/health.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common'
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
-import { Response } from 'express'
+import { Controller, Get, HttpStatus, Res } from "@nestjs/common";
+import { ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Response } from "express";
 
-
-@ApiTags('health')
-@Controller('health')
+@ApiTags("health")
+@Controller()
 export class HealthController {
-  constructor(
+  constructor() {}
 
-  ) {}
-
-  @Get('')
-  @ApiOkResponse({ description: 'Returned if it is reachable.' })
-  @ApiOperation({ description: 'Simple health check to check if an instance is reachable on the network.' })
+  @Get("/health")
+  @ApiOkResponse({ description: "Returned if it is reachable." })
+  @ApiOperation({
+    description:
+      "Simple health check to check if an instance is reachable on the network.",
+  })
   async check(@Res() res: Response) {
-    res.status(HttpStatus.OK).send('OK')
+    res.status(HttpStatus.OK).send("OK");
   }
 }
