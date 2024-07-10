4c25d48dfa77726c79724bd80baf3a5ff2abcf6d
seungminkim <ksm8797@naver.com>
2024-07-10 23:11:53 +0900

    feature : fix server configuration to db config

--- apps/chatting-server/src/config/configurations.ts
+++ apps/chatting-server/src/config/configurations.ts
@@ -1,3 +1,24 @@
 export default () => ({
   env: process.env.NODE_ENV || "local",
+  database: {
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    user: process.env.MYSQL_ROOT_USER || "root",
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    name: process.env.DB_NAME,
+    timezone: process.env.DB_TIMEZONE || "Z",
+    slave: {
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      user: process.env.MYSQL_ROOT_USER,
+      password: process.env.MYSQL_ROOT_PASSWORD || "",
+      name: process.env.DB_NAME,
+      timezone: process.env.DB_TIMEZONE || "Z",
+    },
+    config: {
+      connectionLimit: parseInt(process.env.connectionLimit) || 5,
+      maxIdle: parseInt(process.env.maxIdle) || 5,
+      idleTimeout: parseInt(process.env.idleTimeout) || 7210000,
+    },
+  },
 });
