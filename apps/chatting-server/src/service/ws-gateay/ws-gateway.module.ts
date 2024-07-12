3319814215b48f43b862b7116dd1e2fc0c4b0dfa
seungminkim <ksm8797@naver.com>
2024-07-13 01:28:36 +0900

    feature : add chattingMessageService and chattingRoomUserService service import and Add asynchronous storage of data in mysql

--- apps/chatting-server/src/service/ws-gateay/ws-gateway.module.ts
+++ apps/chatting-server/src/service/ws-gateay/ws-gateway.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { WsGateway } from "./ws-gateway";
 import { WsGatewayService } from "./ws-gateway.service";
 import { WsGatewayController } from "./ws-gateway.controller";
+import { ChattingMessageModule } from "../chatting-message/chatting-message.module";
+import { ChattingRoomUserModule } from "../chatting-room-user/chatting-room-user.module";
 
 @Module({
-  imports: [],
+  imports: [ChattingMessageModule, ChattingRoomUserModule],
   controllers: [WsGatewayController],
   providers: [WsGateway, WsGatewayService],
 })
--- apps/chatting-server/src/service/ws-gateay/ws-gateway.ts
+++ apps/chatting-server/src/service/ws-gateay/ws-gateway.ts
@@ -8,6 +8,12 @@ import {
 } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 import { HttpStatus } from "@nestjs/common";
+import { ChattingMessageService } from "../chatting-message/chatting-message.service";
+import { ChattingRoomUserService } from "../chatting-room-user/chatting-room-user.service";
+
+interface UserRoomMap {
+  [userId: string]: Set<string>;
+}
 
 @WebSocketGateway({
   cors: {
@@ -20,22 +26,121 @@ export class WsGateway
 {
   @WebSocketServer()
   server: Server;
+  private userRooms: UserRoomMap = {};
 
-  constructor() {}
+  constructor(
+    private chattingMessageService: ChattingMessageService,
+    private chattingRoomUserService: ChattingRoomUserService,
+  ) {}
 
   @SubscribeMessage("join")
-  join(client: Socket | any) {
-    console.log("client_connect!!", client.user?.id);
-    client.join(client.user?.id);
+  join(
+    client: Socket,
+    payload: { chattingRoomId: string; userId: string; username: string },
+  ) {
+    if (!this.userRooms[payload.userId]) {
+      this.userRooms[payload.userId] = new Set();
+    }
+
+    const currentUserRooms = this.userRooms[payload.userId];
+
+    // 해당 채팅방에 같은 아이디로 이미 연결 되어 있는 경우
+    if (currentUserRooms.has(payload.chattingRoomId)) {
+      // 연결 내역 삭제 후 재 연결
+      if (this.userRooms[payload.userId]) {
+        this.userRooms[payload.userId].delete(payload.chattingRoomId);
+        if (this.userRooms[payload.userId].size === 0) {
+          delete this.userRooms[payload.userId];
+        }
+      }
+
+      this.chattingRoomUserService
+        .removeChattingRoomUser({
+          requestUserId: payload.userId,
+          chattingRoomId: payload.chattingRoomId,
+        })
+        .catch((e) => {
+          return e;
+        });
+    }
+
+    // Client 연결
+    client.join(
+      `chattingRoomId:${payload.chattingRoomId}:userId:${payload.userId}`,
+    );
+    currentUserRooms.add(payload.chattingRoomId);
+
     this.server
-      .to(client.user?.id)
-      .emit("connect", { statusCode: HttpStatus.OK, message: ["success"] });
+      .to(`chattingRoomId:${payload.chattingRoomId}:userId:${payload.userId}`)
+      .emit("user-joined", {
+        statusCode: HttpStatus.OK,
+        message: ["success"],
+        userId: payload.userId,
+        username: payload.username,
+      });
+
+    // DB에는 비동기로 생성
+    this.chattingRoomUserService
+      .createChattingRoomUser({
+        requestUserId: payload.userId,
+        chattingRoomId: payload.chattingRoomId,
+      })
+      .catch((e) => {
+        return e;
+      });
   }
 
   @SubscribeMessage("leave")
-  leave(client: Socket | any) {
-    console.log("LEAVE");
-    client.leave(client.user.id);
+  leave(
+    client: Socket,
+    payload: { chattingRoomId: string; userId: string; username: string },
+  ) {
+    client.leave(payload.chattingRoomId);
+    if (this.userRooms[payload.userId]) {
+      this.userRooms[payload.userId].delete(payload.chattingRoomId);
+      if (this.userRooms[payload.userId].size === 0) {
+        delete this.userRooms[payload.userId];
+      }
+    }
+
+    this.server
+      .to(`chattingRoomId:${payload.chattingRoomId}:userId:${payload.userId}`)
+      .emit("user-left", {
+        userId: payload.userId,
+        username: payload.username,
+      });
+
+    this.chattingRoomUserService
+      .removeChattingRoomUser({
+        requestUserId: payload.userId,
+        chattingRoomId: payload.chattingRoomId,
+      })
+      .catch((e) => {
+        return e;
+      });
+  }
+
+  @SubscribeMessage("send-message")
+  handleMessage(
+    client: Socket,
+    payload: {
+      message: string;
+      chattingRoomId: string;
+      userId: string;
+      username: string;
+    },
+  ) {
+    this.server.to(payload.chattingRoomId).emit("receive-message", payload);
+
+    this.chattingMessageService
+      .createChattingMessage({
+        content: payload.message,
+        userId: payload.userId,
+        chattingRoomId: payload.chattingRoomId,
+      })
+      .catch((e) => {
+        return e;
+      });
   }
 
   handleConnection(client: any, ...args: any[]) {
